14d2cc63c1ec83601a8572437bd48a30bfefbf9b
nguyenquang2002 <nguyenquang22062002@gmail.com>
2023-09-27 01:29:04 +0700

    Double Jump and Dash

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -7,15 +7,46 @@ public class PlayerController : MonoBehaviour
     [Header("Horizontal Movement Settings")]
     [SerializeField] float speed = 4;
 
-    private float velX, velY;
-    private Rigidbody2D rb;
-    private Animator animator;
-
-    [Header("Jump Settings")]
+    [Header("Vertical Movement Settings")]
     [SerializeField] float jumpForce = 7;
+    private int jumpBufferCounter = 0;
+    [SerializeField] int jumpBufferFrames;
+    private float cyoteTimeCounter = 0;
+    [SerializeField] float coyteTime;
+    private int airJumpCounter = 0;
+    [SerializeField] int maxAirJump;
+
+    [Header("Ground Check Settings")]
     [SerializeField] Transform groundCheckPoint;
     [SerializeField] float groundCheckRadius = 0.2f;
     [SerializeField] LayerMask whatIsGround;
+
+    [Header("Dash Settings")]
+    [SerializeField] float dashSpeed;
+    [SerializeField] float dashTime;
+    [SerializeField] float dashCooldown;
+
+    private PlayerStateList pState;
+    private float velX, velY;
+    private float gravity;
+    private Rigidbody2D rb;
+    private Animator animator;
+    private bool canDash = true;
+    private bool dashed;
+
+    public static PlayerController Instance;
+
+    private void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -23,15 +54,23 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
 
         animator = GetComponent<Animator>();
+
+        pState = GetComponent<PlayerStateList>();
+
+        gravity = rb.gravityScale;
     }
 
     // Update is called once per frame
     void Update()
     {
         GetInputs();
-        Run();
+        UpdateJumpVariable();
+
+        if (pState.dashing) return;
         Flip();
+        Run();
         Jump();
+        StartDash();
     }
     private void GetInputs()
     {
@@ -52,18 +91,84 @@ public class PlayerController : MonoBehaviour
             transform.localScale = new Vector3(1, transform.localScale.y);
         }
     }
+    private void StartDash()
+    {
+        if(Input.GetButtonDown("Dash") && canDash && !dashed)
+        {
+            StartCoroutine(Dash());
+            dashed = true;
+        }
+
+        if (Grounded())
+        {
+            dashed = false;
+        }
+    }
+    IEnumerator Dash()
+    {
+        canDash = false;
+        pState.dashing = true;
+        animator.SetTrigger("Dashing");
+        animator.SetBool("Running", false);
+        animator.SetBool("Jumping", false);
+        rb.gravityScale = 0;
+        rb.velocity = new Vector2(transform.localScale.x * dashSpeed, 0);
+        yield return new WaitForSeconds(dashTime);
+        rb.gravityScale = gravity;
+        pState.dashing = false;
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
+    }
     public bool Grounded()
     {
         return Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatIsGround);
     }
     private void Jump()
     {
-        if(Input.GetButtonDown("Jump") && Grounded())
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-            
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+            pState.jumping = false;
+        }
+
+        if (!pState.jumping)
+        {
+            if (jumpBufferCounter > 0 && cyoteTimeCounter > 0)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                pState.jumping = true;
+            }
+            else if(!Grounded() && Input.GetButtonDown("Jump") && airJumpCounter < maxAirJump)
+            {
+                pState.jumping = true;
+                airJumpCounter++;
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            }
         }
         animator.SetBool("Jumping", !Grounded());
         animator.SetFloat("yVelocity", rb.velocity.y);
+    }
+
+    private void UpdateJumpVariable()
+    {
+        if (Grounded())
+        {
+            pState.jumping = false;
+            cyoteTimeCounter = coyteTime;
+            airJumpCounter = 0;
+        }
+        else
+        {
+            cyoteTimeCounter -= Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpBufferCounter = jumpBufferFrames;
+        }
+        else
+        {
+            jumpBufferCounter--;
+        }
     }
 }
